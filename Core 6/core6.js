cbeec773f54256e31c3299f19bd7635fa5d7a04c
edeni4458 <edeni4458@gmail.com>
2022-12-19 23:21:06 -0500

    yo

---
+++ Core 6/core6.js
@@ -0,0 +1,10 @@
+function changeTo(){
+    var swap = document.querySelector('#logOut')
+    swap.innerText = 'Logout'
+}
+
+function hideIt(){
+    var hidIt = document.querySelector('#removIt');
+    hidIt.remove()
+}
+
